3340bd5000b5f5e621eb283437c5ecc1ba9d0cc4
silverawz <deroussenicolas@gmail.com>
2020-06-26 00:10:25 +0200

    #6 WaitingListServiceUnitTest completed

--- Bibliotheque-microservice/src/test/java/com/deroussenicolas/service/unit/test/WaitingListServiceUnitTest.java
+++ Bibliotheque-microservice/src/test/java/com/deroussenicolas/service/unit/test/WaitingListServiceUnitTest.java
@@ -16,7 +16,9 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.deroussenicolas.dao.WaitingListReservationRepository;
+import com.deroussenicolas.entities.Book;
 import com.deroussenicolas.entities.WaitingListReservation;
+import com.deroussenicolas.service.BookService;
 import com.deroussenicolas.service.WaitingListReservationServiceImpl;
 
 @SpringBootTest
@@ -27,24 +29,50 @@ public class WaitingListServiceUnitTest {
 	private static WaitingListReservationServiceImpl waitingListReservationServiceImpl;
 	private static WaitingListReservation waitingListReservation;
 	private static List<WaitingListReservation> waitingListReservationList;
+	private static List<Book> bookList;
 	private static WaitingListReservationRepository waitingListRepository = mock(WaitingListReservationRepository.class);
+	private static BookService bookService = mock(BookService.class);
 	
     @BeforeClass
     public static void initBeforeClass() {
     	waitingListReservationServiceImpl = new WaitingListReservationServiceImpl();
-    	creatingTheWaitingListReservationList();
+    	creatingTheWaitingListReservationListAndTheBookList();
     	given(waitingListRepository.findAll()).willReturn(waitingListReservationList);
     	given(waitingListRepository.waitingListReservationOfBookWithParams(1, false, false)).willReturn(waitingListReservationList);
     	given(waitingListRepository.waitingListReservationOfUserBookWithParamsForASingleObject(1, 1, false, false)).willReturn(waitingListReservation);  	
     	given(waitingListRepository.waitingListReservationById(1)).willReturn(waitingListReservation);
-    	given(waitingListRepository.waitingListReservationOfUserWithParamsArchived(1, false)).willReturn(waitingListReservationList);   	
+    	given(waitingListRepository.waitingListReservationOfUserWithParamsArchived(1, false)).willReturn(waitingListReservationList);      	  	
+    	given(waitingListRepository.waitingListReservationOfUserWithParams(1, false, false)).willReturn(waitingListReservationList);   
+    	given(bookService.findAll()).willReturn(bookList);   
     }
     
-    private static void creatingTheWaitingListReservationList() {
+    private static void creatingTheWaitingListReservationListAndTheBookList() {
+    	bookList = new ArrayList<>();
+    	Book book1 = new Book();
+    	book1.setId_book(1);   	
+    	bookList.add(book1);
+    	
+    	Book book2 = new Book();
+    	book2.setId_book(2);
+    	bookList.add(book2);
+    	
+    	Book book3 = new Book();
+    	book3.setId_book(3);
+    	bookList.add(book3);
+    	
+    	
     	waitingListReservationList = new ArrayList<>();
-    	waitingListReservationList.add(new WaitingListReservation());
-    	waitingListReservationList.add(new WaitingListReservation());
-    	waitingListReservationList.add(new WaitingListReservation());
+    	WaitingListReservation waitingListReservation1 = new WaitingListReservation();
+    	waitingListReservation1.setBook(book1);
+    	waitingListReservationList.add(waitingListReservation1);
+    	
+    	WaitingListReservation waitingListReservation2 = new WaitingListReservation();
+    	waitingListReservation2.setBook(book2);    	    	
+    	waitingListReservationList.add(waitingListReservation2);
+    	
+    	WaitingListReservation waitingListReservation3 = new WaitingListReservation();
+    	waitingListReservation3.setBook(book3);   	  	
+    	waitingListReservationList.add(waitingListReservation3);
     }
     
     @Test
@@ -78,16 +106,16 @@ public class WaitingListServiceUnitTest {
     	assertEquals(3, waitingListReservationList.size());
     }
     
-    
-    
-    
-    // TO DO assez compliqué a faire
     @Test 
     public void checkIfUserHasMadeAReservationForEchBooks() {
-    	
-    	//waitingListReservationServiceImpl.checkIfUserHasMadeAReservationForEchBooks();
-    	
-    	
+    	List<Boolean> booleanListParameters = new ArrayList<>();
+    	booleanListParameters.add(false);
+    	booleanListParameters.add(false);
+    	booleanListParameters.add(false); 	
+    	List<Boolean> booleanListResult = waitingListReservationServiceImpl.checkIfUserHasMadeAReservationForEchBooks(booleanListParameters, 1);
+    	assertEquals(booleanListResult.get(0), true);
+    	assertEquals(booleanListResult.get(1), true);
+    	assertEquals(booleanListResult.get(2), true);
     }
     
 }
